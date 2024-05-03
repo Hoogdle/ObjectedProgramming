9ea7338ad1c4d057da92552ed31be55221098e10
Taeyeong Kim <xodud7737@gmail.com>
2024-05-03 17:02:54 +0900

    Assignment

--- homework/homework/Vector.h
+++ homework/homework/Vector.h
@@ -8,52 +8,93 @@ template <class T> class Vector {
 
 public:
   Vector(unsigned capacity = 5) {
+    this->_capacity = capacity;
+    _memory = (T*)calloc(capacity,sizeof(T));
     // _memory의 크기를 매개변수 capacity 크기로 동적으로 할당하고
     // 동적으로 할당된 배열의 요소는 없도록 초기화하는 생성자를 여기에 구현
   }
 
   Vector(unsigned size, const T &element) {
+    this->_size = size;
+    this->_capacity = size;
+    _memory = (T*)malloc(sizeof(T)*size);
+    for(int i=0;i<size;++i){
+      _memory[i] = element;
+    }
     // 메모리의 크기와 요소의 개수를 size 만큼 설정하고 메모리를 생성하고
     // 동적 메모리의 각 요소의 값은 element로 초기화하는 코드를 여기에 구현
   }
 
   unsigned size() const {
+    return _size;
     // 동적 배열에 저장된 요소의 갯수를 리턴
   }
 
   unsigned capacity() const {
+    return _capacity;
     // 동적 배열의 크기를 리턴
   }
 
-  const T &at(unsigned index) const {
+  const T & Vector::at(unsigned index) const {
+    if(index>=capacity){
+      printf("over the capacity!");
+      return;
+    }
+    return _memory[index];
     // 동적 배열의 index 위치에 저장된 요소를 리턴
     // 배열의 크기를 넘는 인덱스틑 고려하지 않아도 됨
   }
 
   void set(unsigned index, const T &element) {
+    if(index>=capacity){
+      printf("over the capacity!");
+      return;
+    }
+    _memory[index] = element;
     // index가 유효할 경우만 동적 배열의 index 위치의 요소를 element로 변경
     // index 요소가 유효하다는 의미는 배열의 요소의 갯수를 넘지 않는 범위의 인덱스라는 의미
   }
 
   bool empty() const {
+    if(size==0){return true};
+    else {return false};
     // 배열에 저장된 요소가 없을 경우 true, 그렇지 않다면 false를 리턴
   }
 
   void push_back(const T &element) {
     // 배열의 끝에 element를 추가
+    if(_size+1==_capacity){
+      _capacity *= 2;
+      _memory = (T*)realloc(_memory,sizeof(T)*capacity);
+    }
+    ++_size;
+    _memory[_size-1] = element;
   }
 
   void pop_back() {
+    if(empty()){return;}
+    _size -= 1;
+    _memory[_size] == NULL;
     // 배열의 마지막 요소를 제거
     // 더이상 제거할 요소가 없다면 무시
   }
 
   void erase(unsigned index) {
+    if(empty()){return;}
+    if(!(index>=0 && index<size)){return;}
+    for(int i=index;i<_size;++i){_memory[i-1] = _memory[i];}
+    _size -= 1;
     // index가 유효한 경우, 배열의 index 위치의 요소를 삭제
     // 유효하지 않은 경우는 아무 일도 일어나지 않음
   }
 
   void insert(unsigned index, const T &element) {
+    if(!(index>=0 && index<size)){return;}
+    _size += 1;
+    for(int i=_size-1;i>=index;--i){
+      _memory[i] = _memory[i-1];
+    }
+    _memory[index] = element;
     // index 위치가 유효한 경우(배열의 요소의 수를 넘지 않는 위치)
     // index 위치에 element를 삽입
   }
